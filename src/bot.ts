c7960e5215ee13c307a0beee0edce1a9d19443f5
Richard Tochtermann <richard.tochtermann@hotmail.com>
2022-09-05 11:19:26 +1000

    Created directory structure, configured typescript and authenticated with discord using the bot's token

---
+++ src/bot.ts
@@ -0,0 +1,12 @@
+import { Client } from "discord.js";
+import { token } from './config.json';
+
+console.log("Bot is starting...");
+
+const client = new Client({
+    intents: []
+});
+
+client.login(token);
+
+console.log(client);
