46127a0d87759e0bc7e5ddc99e5a321a847147fd
Richard Tochtermann <richard.tochtermann@hotmail.com>
2022-09-08 12:25:38 +1000

    Refactored executePing function in src/commands/ping.ts so it is less difficuto design the tests for ping slash command and re-designed tests in src/tests/ping.test.ts to correctly test executePing function after components and embeds were added in

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -5,18 +5,19 @@ import {
   CacheType, 
   ChatInputCommandInteraction, 
   Client,
-  EmbedBuilder
+  EmbedBuilder,
+  messageLink
 } from 'discord.js';
 
 export const Ping: Command = {
   name: 'ping',
   description: 'Replies with "Pong!" (used for checking if bot is alive)',
-  run: async (client: Client, interaction: ChatInputCommandInteraction<CacheType>) => {
+  run: async (client: Client, interaction: ChatInputCommandInteraction<CacheType>): Promise<void> => {
     await executePing(client, interaction);
   }
 };
 
-export const executePing = async (client: Client, interaction: ChatInputCommandInteraction<CacheType>) => {
+export const executePing = async (client: Client, interaction: ChatInputCommandInteraction<CacheType>): Promise<void> => {
   const button = buttons.find(b => b.buttonId === buttonId);
   const row = button.row;
   
@@ -26,12 +27,12 @@ export const executePing = async (client: Client, interaction: ChatInputCommandI
   .setColor(0x0099FF)
   .setTitle('Pong!');
 
-  interaction.deferReply().then(async (resultMessage) => {
-    const botLatency = resultMessage.interaction.createdTimestamp - interaction.createdTimestamp;
-    const apiLatency = client.ws.ping;
-    embed.setDescription(
-      `Bot latency: ${botLatency}ms\nAPI latency: ${apiLatency}ms`
-    );
-    await interaction.editReply({ content: '', components: [row], embeds: [embed] })
-  });
+  const msg = await interaction.deferReply();
+  const botLatency = msg.interaction.createdTimestamp - interaction.createdTimestamp;
+  const apiLatency = client.ws.ping;
+  embed.setDescription(
+    `Bot latency: ${botLatency}ms\nAPI latency: ${apiLatency}ms`
+  );
+
+  await interaction.editReply({ content: '', components: [row], embeds: [embed] });
 };
--- src/tests/ping.test.ts
+++ src/tests/ping.test.ts
@@ -1,24 +1,104 @@
 import { Ping, executePing } from '../commands/ping';
-import { CacheType, ChatInputCommandInteraction } from 'discord.js';
+import { Button } from '../buttons/button-interface';
+import { ActionRowBuilder, 
+  ButtonInteraction, 
+  ButtonBuilder,
+  CacheType, 
+  ChatInputCommandInteraction, 
+  Client, 
+  EmbedBuilder, 
+  GuildTextBasedChannel, 
+  InteractionCollector, 
+  InteractionResponse, 
+  Message, 
+  SelectMenuInteraction, 
+  WebhookEditMessageOptions 
+} from 'discord.js';
 
 describe('ping command', () => {
+  const messageBoolean = ({} as unknown) as Message<boolean>;
+
+  const interactionResponse = ({
+    interaction: {
+      createdTimestamp: Number,
+    }
+  } as unknown) as InteractionResponse<boolean>;
+
+  const interactionCollector = ({
+    on: jest.fn(),
+  } as unknown) as InteractionCollector<SelectMenuInteraction<CacheType> | ButtonInteraction<CacheType>>;
+
   const interaction = ({
-    reply: jest.fn(),
+    deferReply: jest.fn(() => { return interactionResponse; }),
+    editReply: jest.fn(() => { return messageBoolean; }),
+    channel: {
+      createMessageComponentCollector: jest.fn(() => { return interactionCollector; }),
+    },
+    createdTimestamp: Number,
   } as unknown) as ChatInputCommandInteraction<CacheType>;
+
+  const client = ({
+    intents: [],
+    ws: {
+      ping: Number,
+    }
+  } as unknown) as Client;
+
+  // const button = ({
+  //   handleInteraction: jest.fn(),
+  // } as unknown) as Button;
+
+  // const buttonArr = ({
+  //   find: jest.fn(() => { return button; }),
+  // } as unknown) as Button[];
+
+  // const guildTextBasedChannel = ({} as unknown) as GuildTextBasedChannel;
 
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  test('executePing replies with a string (called using slash command run function)', () => {
-    Ping.run(interaction);
-    expect(interaction.reply).toHaveBeenCalledTimes(1);
-    expect(interaction.reply).toHaveBeenCalledWith(expect.any(String));
+  // The 3 tests below may be pointless...
+
+  // test('executePing calls buttons.find once (executePing directly called)', async () => {
+  //   await executePing(client, interaction);
+  //   expect(buttonArr.find).toHaveBeenCalledTimes(1);
+  //   expect(buttonArr.find).toHaveBeenCalledWith(button);
+  // });
+
+  // test('executePing calls buttons.handleInteraction once (executePing directly called)', async () => {
+  //   await executePing(client, interaction);
+  //   expect(button.handleInteraction).toHaveBeenCalledTimes(1);
+  //   expect(button.handleInteraction).toHaveBeenCalledWith(guildTextBasedChannel);
+  // });
+
+  // test('executePing creates embed (called using slash command run function)', () => {
+  //   Ping.run(client, interaction);
+  //   expect(interaction.deferReply).toHaveBeenCalledTimes(1);
+  //   expect(interaction.editReply).toHaveBeenCalledTimes(1);
+  //   expect(interaction.editReply).toHaveBeenCalledWith(webhookEditMessageOptions);
+  // });
+
+  test('executePing correctly calls deferReply and editReply (executePing directly called)', async () => {
+    await executePing(client, interaction);
+    expect(interaction.deferReply).toHaveBeenCalledTimes(1);
+    expect(interaction.editReply).toHaveBeenCalledTimes(1);
+    expect(interaction.editReply).toHaveBeenCalledWith({ 
+      content: '', 
+      components: expect.any(Object), 
+      embeds: expect.any(Object),
+    });
   });
 
-  test('executePing replies with a string (executePing directly called)', () => {
-    executePing(interaction);
-    expect(interaction.reply).toHaveBeenCalledTimes(1);
-    expect(interaction.reply).toHaveBeenCalledWith(expect.any(String));
+  test('executePing correctly calls deferReply and editReply (called using slash command run function)', async () => {
+    // test breaks w/o await below
+    await Ping.run(client, interaction);
+    expect(interaction.deferReply).toHaveBeenCalledTimes(1);
+    expect(interaction.editReply).toHaveBeenCalledTimes(1);
+    expect(interaction.editReply).toHaveBeenCalledWith({ 
+      content: '', 
+      components: expect.any(Object), 
+      embeds: expect.any(Object),
+    });
   });
 });
