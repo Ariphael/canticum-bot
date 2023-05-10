f376a13e2daeb100bbe6e8d5c2c2b5a421370bab
Richard Tochtermann <richard.tochtermann@hotmail.com>
2023-05-10 16:42:40 +1000

    Corrected spotify access token retrieval and renewal procedures as the Authorization Code flow with PKCE extension will be utilised instead

--- src/bot.ts
+++ src/bot.ts
@@ -1,10 +1,15 @@
 import fs from 'fs';
+import * as dotenv from 'dotenv';
+import * as cron from 'node-cron';
 import { Client, Collection, GatewayIntentBits } from 'discord.js';
-import { token } from './config.json';
-// const token = '0123456789'; // fake token
 import { addReadyEventToClient } from './events/ready';
 import { addInteractionCreateEventToClient } from './events/interactionCreate';
 import { Command } from './interfaces/command-interface';
+import axios from 'axios';
+
+dotenv.config();
+
+var refresh_token;
 
 const client = new Client({
   intents: [
@@ -16,11 +21,14 @@ const client = new Client({
 export const startCanticum = async (client: Client<boolean>) => {
   console.log('Bot is starting...');
 
+  const spotify_access_token = await acquireSpotifyAccessToken();
+  storeSpotifyAccessToken(spotify_access_token);
+  scheduleHourlySpotifyAccessTokenRenewal();
   const commandCollection = await getCommands();
   addReadyEventToClient(client, commandCollection);
   addInteractionCreateEventToClient(client, commandCollection);
 
-  await client.login(token);
+  await client.login(process.env.DISCORD_TOKEN);
 };
 
 const getCommands = async (): Promise<Collection<string, Command>> => {
@@ -36,5 +44,47 @@ const getCommands = async (): Promise<Collection<string, Command>> => {
 
   return commandCollection;
 };
+
+const scheduleHourlySpotifyAccessTokenRenewal = () => {
+  const currentMinutes = new Date().getMinutes();
+  cron.schedule(`${currentMinutes} * * * *`, () => {
+    console.log("refreshing spotify access token...");
+    const bodyParams = {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token,
+      client_id: process.env.SPOTIFY_API_ID
+    }
+    const spotifyAccessTokenRenewalRequestResponse = 
+      axios.post('https://accounts.spotify.com/api/token', 
+        bodyParams, {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        }
+      )
+        .then(response => storeSpotifyAccessToken(response.data.access_token));
+    
+  });
+}
+
+const acquireSpotifyAccessToken = async (): Promise<string> => {
+  const spotifyAccessTokenRequestResponse =
+    await axios.post(
+      `https://accounts.spotify.com/api/token?grant_type=authorization_code&code=${process.env.SPOTIFY_AUTH_CODE}&redirect_uri=${process.env.SPOTIFY_REDIRECT_URI}&client_id=${process.env.SPOTIFY_API_ID}&code_verifier=${process.env.CODE_VERIFIER}`
+    );
+
+  refresh_token = spotifyAccessTokenRequestResponse.data.refresh_token;
+  return spotifyAccessTokenRequestResponse.data.access_token;
+}
+
+const storeSpotifyAccessToken = (access_token: string) => {
+  const accessTokenJsonString = JSON.stringify({
+    spotifyAccessToken: access_token
+  });
+
+  fs.writeFile('../config/config.json', accessTokenJsonString, 'utf8', (err) => {
+    if (err) throw err;
+  });
+}
 
 startCanticum(client);
