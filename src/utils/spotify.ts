0cdcc39218d8f029ead11f390f219578ba55640e
Richard Tochtermann <richard.tochtermann@hotmail.com>
2023-05-15 17:19:38 +1000

    Fixed issues involving the refreshTokenRevocationExceptionHandler, relied on user input to acquire the authorization code required in the access tokken request step , used promised-based primitives (async, await) to ensure refreshTokenRevocationException properly makes the execution thread wait and made functions that are essential to the Authorization Code with PKCE flow public

--- src/utils/spotify.ts
+++ src/utils/spotify.ts
@@ -1,8 +1,139 @@
 import * as cron from 'node-cron';
+import * as path from 'path';
 import axios from "axios";
 import fs from 'fs';
 import puppeteer from 'puppeteer';
+import { input } from '@inquirer/prompts';
 import { createHash } from 'node:crypto';
+
+export const refreshSpotifyAccessToken = async () => {
+  const bodyParams = {
+    grant_type: 'refresh_token',
+    refresh_token: process.env['SPOTIFY_REFRESH_TOKEN'],
+    client_id: process.env['SPOTIFY_API_ID']
+  }
+
+  const configParams = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    }
+  }
+
+  await axios
+    .post('https://accounts.spotify.com/api/token', bodyParams, configParams)
+    .then(
+      response => storeSpotifyAccessToken(response.data.access_token)
+    )
+    .catch(async (error) => {
+      if (error.response !== undefined && error.response.data.error === 'invalid_grant') {
+        await refreshTokenRevocationExceptionHandler();
+      } else if (error.request) {
+        console.log('Network error. Attempting to refresh spotify access token again...');
+        refreshSpotifyAccessToken();
+      } else {
+        console.log(error);
+      }
+    });
+}
+
+export const scheduleHourlySpotifyAccessTokenRenewal = () => {
+  const currentMinutes = new Date().getMinutes();
+  cron.schedule(`${currentMinutes} * * * *`, async () => {
+    console.log("refreshing spotify access token...");
+    const bodyParams = {
+      grant_type: 'refresh_token',
+      refresh_token: process.env['SPOTIFY_REFRESH_TOKEN'],
+      client_id: process.env['SPOTIFY_API_ID']
+    };
+
+    const configParams = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    };
+
+    await axios
+      .post('https://accounts.spotify.com/api/token', bodyParams, configParams)
+      .then(response => storeSpotifyAccessToken(response.data.access_token))
+      .catch(async (error) => {
+        if (error.response && error.response.data.error === 'invalid_grant') {
+          await refreshTokenRevocationExceptionHandler();
+        }
+      });
+    
+  });
+}
+ 
+export const refreshTokenRevocationExceptionHandler = async () => {
+  console.log('refreshTokenRevocationExceptionHandler called...');
+
+  const codeChallenge = generateCodeChallenge(process.env['SPOTIFY_CODE_VERIFIER']);
+  const userAuthorizationRequestQueryParameters = new URLSearchParams({
+    client_id: process.env['SPOTIFY_API_ID'],
+    response_type: 'code',
+    redirect_uri: process.env['SPOTIFY_REDIRECT_URI'],
+    code_challenge_method: 'S256',
+    code_challenge: codeChallenge,
+  });
+  const requestURL = 
+    `https://accounts.spotify.com/authorize?${userAuthorizationRequestQueryParameters.toString()}`;
+
+  console.log('Instructions: ');
+  console.log(`Please open a browser and go to the URL: ${requestURL}`);
+  console.log(
+    'Press the accept button, then copy the code parameter in the redirect URI to clipboard'
+  );
+
+  const responseAuthCode = await input({
+    message: 'Enter authorization code',
+  });
+  requestAccessToken(responseAuthCode);
+}
+
+const requestAccessToken = (authCode: string) => {
+  doRequestAccessToken(authCode, 0);
+}
+
+const doRequestAccessToken = (authCode: String, attempt: number) => {
+  const bodyParams = {
+    grant_type: 'authorization_code',
+    code: authCode,
+    redirect_uri: process.env['SPOTIFY_REDIRECT_URI'],
+    client_id: process.env['SPOTIFY_API_ID'],
+    code_verifier: process.env['SPOTIFY_CODE_VERIFIER']
+  };
+
+  const configParams = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  };
+
+  axios
+    .post('https://accounts.spotify.com/api/token', bodyParams, configParams)
+    .then((response) => {
+      process.env['SPOTIFY_REFRESH_TOKEN'] = response.data.refresh_token;
+      storeSpotifyAccessToken(response.data.access_token);
+      console.log('Spotify API refresh and access token updated :)');
+    })
+    .catch((error) => {
+      if (error.response && error.response.data.error === 'invalid_grant') {
+        console.log(
+          `Please input an unused authorization code. Error ${error.code}: ${error.response.data.error}`
+        );
+      } else if (error.request) {
+        if (attempt === 11) {
+          console.log("Attempted 11 times. Requesting access token terminated.");
+          return;
+        }
+        console.log(`Network error occurred. Retrying... (attempt ${attempt + 1}/11)`);
+        doRequestAccessToken(authCode, attempt + 1);
+      } else {
+        console.log('An unexpected error occurred while attempting to request an access token');
+        console.log(error);
+      }
+    });
+}
 
 const generateCodeChallenge = (codeVerifier: string) => {
   const encoder = new TextEncoder();
@@ -13,25 +144,8 @@ const generateCodeChallenge = (codeVerifier: string) => {
     .replace(/\+/g, '-')
     .replace(/\//g, '_')
     .replace(/=+$/, '');
-  
-  // const digest = await window.crypto.subtle.digest('SHA-256', data);
 
   return digest;
-}
-
-const acquireSpotifyAccessToken = async (auth_code: string): Promise<string> => {
-  const bodyParams = {
-    grant_type: 'authorization_code',
-    code: auth_code,
-    redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
-    client_id: process.env.CLIENT_ID,
-    code_verifier: process.env.CODE_VERIFIER,
-  };
-  const spotifyAccessTokenRequestResponse = 
-    await axios.post('https://accounts.spotify.com/api/token', bodyParams);
-
-  process.env['REFRESH_TOKEN'] = spotifyAccessTokenRequestResponse.data.refresh_token;
-  return spotifyAccessTokenRequestResponse.data.access_token;
 }
 
 const storeSpotifyAccessToken = (access_token: string) => {
@@ -39,77 +153,12 @@ const storeSpotifyAccessToken = (access_token: string) => {
     spotifyAccessToken: access_token
   });
 
-  fs.writeFile('../config/config.json', accessTokenJsonString, 'utf8', (err) => {
-    if (err) throw err;
-  });
-}
-
-const scheduleHourlySpotifyAccessTokenRenewal = () => {
-  const currentMinutes = new Date().getMinutes();
-  cron.schedule(`${currentMinutes} * * * *`, async () => {
-    console.log("refreshing spotify access token...");
-    const bodyParams = {
-      grant_type: 'refresh_token',
-      refresh_token: process.env['SPOTIFY_REFRESH_TOKEN'],
-      client_id: process.env['SPOTIFY_API_ID']
-    } 
-
-    axios.post('https://accounts.spotify.com/api/token', 
-        bodyParams, {
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          }
-        }
-    )
-      .then(
-        response => storeSpotifyAccessToken(response.data.access_token),
-        reason => {
-          if (reason.error === 'invalid_grant') {
-            //... refreshTOkenRevocationExceptionHandler
-          }
-        }
-      );
-    
-  });
-}
-
-const refreshTokenRevocationExceptionHandler = async () => {
-  const codeChallenge = generateCodeChallenge(process.env['SPOTIFY_CODE_VERIFIER']);
-  const userAuthorizationRequestQueryParameters = new URLSearchParams({
-    client_id: process.env['SPOTIFY_API_ID'],
-    response_type: 'code',
-    redirect_uri: process.env['SPOTIFY_REDIRECT_URI'],
-    code_challenge_method: 'S256',
-    code_challenge: codeChallenge,
-  });
-  const requestURL = 
-    `https://api.spotify.com/authorize?${userAuthorizationRequestQueryParameters.toString()}`;
-
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
-  await page.goto(requestURL);
-  await page.click('body >>> [data-testid=auth-accept]');
-
-  const redirect_uri_code_parameter = new URL(page.url()).searchParams.get('code');
-
-  const newAccessToken: string | null = await axios.post(
-      'https://accounts.spotify.com/api/token', {
-        grant_type: 'authorization_code',
-        code: redirect_uri_code_parameter,
-        redirect_uri: process.env['SPOTIFY_REDIRECT_URI'],
-        client_id: process.env['SPOTIFY_API_ID'],
-        code_verifier: process.env['SPOTIFY_CODE_VERIFIER']
-      })
-    .then(
-      response => {
-        process.env['SPOTIFY_REFRESH_TOKEN'] = response.data.refresh_token;
-        return response.data.access_token;
-      },
-      reason => {
-        console.error(`${reason.error}: ${reason.error_description}`);
-        return null;
-      });
-  
-  return newAccessToken;
+  fs.writeFile(
+    path.join(__dirname, '../../config/spotify.json'), 
+    accessTokenJsonString, 
+    'utf8', 
+    (err) => {
+      if (err) throw err;
+    }
+  );
 }
