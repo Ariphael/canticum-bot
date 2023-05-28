e4c01825c106de7ada0aa93454110b631aff5d40
Richard Tochtermann <richard.tochtermann@hotmail.com>
2023-05-28 18:54:03 +1000

    Added support for enqueuing spotify albums. Implemented functions enqueueSpotifyAlbumRequest and related helper functions including enqueueSpotifyAlbumItems which has substantially similar logic to the playlist functions.

--- src/commands/utils/play/spotify.ts
+++ src/commands/utils/play/spotify.ts
@@ -3,8 +3,10 @@ import { musicQueue } from "../../../queue/musicQueue";
 import { EmbedBuilder } from "discord.js";
 
 const spotifyAccessToken = process.env['SPOTIFY_ACCESS_TOKEN'];
-const emptySpotifyPlaylistErrorStr = 'Unable to enqueue: empty spotify playlist.';
-const maxNoAttemptsPlaylistItemFetch = 2;
+const emptySpotifyPlaylistErrorStr = 'Spotify playlist is empty';
+const emptySpotifyAlbumErrorStr = 'Spotify album is empty';
+const spotifyApiErrorStr = 'Spotify API did not respond';
+const maxNoAttemptsItemFetch = 2;
 
 export const enqueueSpotifyPlaylistRequest = async (url: string): Promise<EmbedBuilder> => {
   const embed = new EmbedBuilder();
@@ -35,6 +37,37 @@ export const enqueueSpotifyPlaylistRequest = async (url: string): Promise<EmbedB
     }
     throw new Error(error.message);
   }
+  return embed;
+}
+
+export const enqueueSpotifyAlbumRequest = async (url: string) => {
+  const embed = new EmbedBuilder();
+  const pathname = new URL(url).pathname;
+  const albumId = pathname.slice(pathname.lastIndexOf('/') + 1);
+
+  try {
+    await axios
+      .get(`https://api.spotify.com/v1/albums/${albumId}`, {
+        headers: {
+          'Authorization': `Bearer ${spotifyAccessToken}`,
+          'Content-Type': 'application/json',
+        }
+      })
+      .then(async (spotifyAlbumInfo) => {
+        if (spotifyAlbumInfo.data.tracks.total === 0)
+          throw new Error(emptySpotifyAlbumErrorStr);
+        await enqueueSpotifyAlbumItems(spotifyAlbumInfo.data.id);
+        embed
+          .setTitle(`${spotifyAlbumInfo.data.name}`)
+          .setDescription(`Enqueued ${spotifyAlbumInfo.data.tracks.total} songs to the queue`)
+          .setURL(url)
+          .setTimestamp();
+      });
+  } catch (reason) {
+    if (reason.request) throw new Error(spotifyApiErrorStr);
+    throw new Error(`${reason.response.error.status} - ${reason.response.error.message}`);
+  }
+
   return embed;
 }
 
@@ -78,16 +111,19 @@ export const enqueueSpotifyTrackRequest = async (url: string): Promise<EmbedBuil
 }
 
 const enqueueSpotifyPlaylistItems = async (playlistId: string) => {
-  var playlistItems = await axios
-    .get(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
-      headers: {
-        'Authorization': `Bearer ${spotifyAccessToken}`,
-        'Content-Type': 'application/json',
-      }
-    })
-    .catch(error => {
-      throw new Error(`${error.status} - ${error.message}`);
-    });
+  try {
+    var playlistItems = await axios.get(
+      `https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+        headers: {
+          'Authorization': `Bearer ${spotifyAccessToken}`,
+          'Content-Type': 'application/json',
+        }
+      });
+  } catch (error) {
+    if (error.request) throw new Error(spotifyApiErrorStr);
+    else if (error.response) throw new Error(`${error.status} - ${error.message}`);
+    throw new Error(error.message);
+  }
 
   do {
     playlistItems.data.items.forEach(async (playlistItem) => {
@@ -102,10 +138,9 @@ const enqueueSpotifyPlaylistItems = async (playlistId: string) => {
             'Content-Type': 'application/json',
             }
           })
-          .catch((error) => {
-            console.log(error);
+          .catch(() => {
             throw new Error(
-              'Failed to get next page of spotify playlist items. Please try command again.'
+              'Failed to get next page of spotify playlist. Please try command again.'
             );
           });
   } while (playlistItems !== undefined);
@@ -131,11 +166,75 @@ const doEnqueueSpotifyPlaylistItem = async (playlistItem, attempt: number) => {
         });
       });
   } catch (error) {
-    console.log(`${attempt + 1}: ${playlistItem.track.name} ${playlistItem.track.artists[0].name}`);
-    if (attempt === maxNoAttemptsPlaylistItemFetch) 
+    if (attempt === maxNoAttemptsItemFetch) 
       throw new Error(
         `Failed to get song ${playlistItem.track.name} ${playlistItem.track.artists[0].name} from playlist. Please try the command again`
       );
     await doEnqueueSpotifyPlaylistItem(playlistItem, attempt + 1);
+  }
+}
+
+const enqueueSpotifyAlbumItems = async (albumId: string) => {
+  try {
+    var albumItems = await axios.get(`https://api.spotify.com/v1/albums/${albumId}/track`, {
+      headers: {
+        'Authorization': `Bearer ${spotifyAccessToken}`,
+        'Content-Type': 'application/json',
+      }
+    });
+  } catch (error) {
+    if (error.request) throw new Error(spotifyApiErrorStr);
+    else if (error.response) throw new Error(`${error.status} - ${error.message}`);
+    throw new Error(error.message);    
+  }
+
+  do {
+    const next = albumItems.data.next;
+    albumItems.data.items.forEach(async (albumTrack) => {
+      await enqueueSpotifyAlbumItem(albumTrack);
+    });
+    albumItems = next === null
+      ? undefined
+      : await axios
+          .get(albumItems.data.next, {
+            headers: {
+            'Authorization': `Bearer ${spotifyAccessToken}`,
+            'Content-Type': 'application/json',
+            }
+          })
+          .catch(() => {
+            throw new Error(
+              'Failed to get next page of spotify album. Please try command again.'
+            );
+          });
+
+  } while (albumItems !== null);
+}
+
+const enqueueSpotifyAlbumItem = async (albumItem) => {
+  await doEnqueueSpotifyAlbumItem(albumItem, 0);
+}
+
+const doEnqueueSpotifyAlbumItem = async (albumItem, attempt: number) => {
+  try {
+    // this is also EXPENSIVE....
+    await axios
+      .get(
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&type=video&q=${albumItem.name}%20-%20${albumItem.artists[0].name}%20Lyrics&key=${process.env.YOUTUBE_API_KEY}`
+      )
+      .then(youtubeApiResponse => {
+        musicQueue.enqueue({
+          musicTitle: albumItem.name,
+          musicId: youtubeApiResponse.data.items[0].id.videoId,
+          uploader: albumItem.artists[0].name,
+          originalURL: albumItem.external_urls.spotify,
+        });      
+      })
+  } catch (error) {
+    if (attempt === maxNoAttemptsItemFetch) 
+      throw new Error(
+        `Failed to get song ${albumItem.name} ${albumItem.artists[0].name} from playlist. Please try the command again`
+      );
+    await doEnqueueSpotifyAlbumItem(albumItem, attempt + 1);    
   }
 }
