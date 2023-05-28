7fce162c0954c4a4006bf9c1d3a20709b1a169ec
Richard Tochtermann <richard.tochtermann@hotmail.com>
2023-05-28 17:35:55 +1000

    Modified executePlay in play.ts to reflect the changes in the helper functions by utilising a nested ternary statement (line 59)

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -10,10 +10,10 @@ import {
 import { MusicPlayer } from '../musicplayer/MusicPlayer';
 import { getVoiceConnection } from '@discordjs/voice';
 import { 
-  enqueueSpotifyMusicInfoFromURL, 
-  enqueueYouTubeMusicInfoFromURL, 
-  getAndEnqueueYouTubeVideoInfoFromNonURLQueryAndUpdateEmbed 
-} from './utils/enqueue';
+  enqueueMusicSpotify, 
+  enqueueMusicYouTube, 
+  enqueueMusicYouTubeNonURLQuery
+} from './utils/queue';
 
 const youtubeURLRegExp = 
   /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/;
@@ -21,8 +21,6 @@ const spotifyURLRegExp =
   /^(?:spotify:|(?:https?:\/\/(?:open|play)\.spotify\.com\/))(?:embed)?\/?(playlist|track)(?::|\/)((?:[0-9a-zA-Z]){22})/;
 
 const musicPlayerInstance = MusicPlayer.getMusicPlayerInstance();
-
-const executePlaySpotifyURLErrorStr = 'Invalid Spotify track URL/id or empty/non-public/non-existent playlist. Please verify that the query is correct.';
 
 export const play: Command = {
   name: 'play',
@@ -39,12 +37,13 @@ export const play: Command = {
 };
 
 const executePlay = async (_client: Client, interaction: ChatInputCommandInteraction<CacheType>) => {
-  const embed = new EmbedBuilder();
   const query = interaction.options.getString('query');
   const voiceConnection = getVoiceConnection(interaction.guild.id);
+  const musicQueueOldLength = musicQueue.getLength();
 
   if (voiceConnection === undefined) {
-    embed.setTitle('Error')
+    const embed = new EmbedBuilder()
+      .setTitle('Error')
       .setDescription('Must be connected to voice channel first!');
     await interaction.reply({ content: '', components: [], embeds: [embed], ephemeral: true });
     return;
@@ -54,73 +53,14 @@ const executePlay = async (_client: Client, interaction: ChatInputCommandInterac
   // a substantial amount of time may be consumed by fetching items in a long playlist.
   await interaction.deferReply();
 
-  if (youtubeURLRegExp.test(query)) {
-    await executePlayYouTubeURL(_client, interaction, embed);
-  } else if (spotifyURLRegExp.test(query)) {
-    await executePlaySpotifyURL(_client, interaction, embed);
-  } else {
-    await executePlayYouTubeQuery(_client, interaction, embed);
-  }
-};
+  const isQueryYouTubeURL = youtubeURLRegExp.test(query);
+  const isQuerySpotifyURL = spotifyURLRegExp.test(query);
 
-const executePlayYouTubeURL = async (
-  _client: Client, 
-  interaction: ChatInputCommandInteraction<CacheType>, 
-  embed: EmbedBuilder
-) => {
-  const query = interaction.options.getString('query');
-  const musicQueueOldLength = musicQueue.getLength();
-
-  await enqueueYouTubeMusicInfoFromURL(query, embed);
-
-  if (musicQueueOldLength === 0 && !musicPlayerInstance.isPlayingAudio()) {
-    musicPlayerInstance.playAudio();
-  }
-
-  await interaction.editReply({ 
-    content: '', 
-    components: [], 
-    embeds: [embed],
-  });
-}
-
-const executePlaySpotifyURL = async (
-  _client: Client, 
-  interaction: ChatInputCommandInteraction<CacheType>, 
-  embed: EmbedBuilder
-) => {
-  const query = interaction.options.getString('query');
-  const musicQueueOldLength = musicQueue.getLength();
-
-  const musicQueueNewLength = await enqueueSpotifyMusicInfoFromURL(query, embed);
-  // if (musicQueueOldLength === musicQueueNewLength) {
-  //   return sendErrorMessageToChannel(
-  //     executePlaySpotifyURLErrorStr,
-  //     interaction,
-  //     embed
-  //   );
-  // }
-
-  if (musicQueueOldLength === 0 && !musicPlayerInstance.isPlayingAudio()) {
-    musicPlayerInstance.playAudio();
-  }
-
-  await interaction.editReply({ 
-    content: '', 
-    components: [], 
-    embeds: [embed],
-  });
-}
-
-const executePlayYouTubeQuery = async (  
-  _client: Client, 
-  interaction: ChatInputCommandInteraction<CacheType>, 
-  embed: EmbedBuilder
-) => {
-  const query = interaction.options.getString('query');
-  const musicQueueOldLength = musicQueue.getLength();
-
-  await getAndEnqueueYouTubeVideoInfoFromNonURLQueryAndUpdateEmbed(query, embed);
+  const embed: EmbedBuilder = isQueryYouTubeURL
+    ? await enqueueMusicYouTube(query)
+    : (isQuerySpotifyURL
+      ? await enqueueMusicSpotify(query)
+      : await enqueueMusicYouTubeNonURLQuery(query));
 
   if (musicQueueOldLength === 0 && !musicPlayerInstance.isPlayingAudio()) {
     musicPlayerInstance.playAudio();
