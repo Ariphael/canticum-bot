8e3e0bbf4ba684487ac3fd09cecdebd124efba33
Richard Tochtermann <richard.tochtermann@hotmail.com>
2023-05-29 11:26:53 +1000

    Added shuffle function to musicQueue object which randomizes queue in place using the Fisher-Yates algorithm

--- src/queue/musicQueue.ts
+++ src/queue/musicQueue.ts
@@ -33,6 +33,15 @@ export const musicQueue = {
       ? undefined
       : queue.splice(queuePosA - 1, queuePosB - 1 - queuePosA + 2);
   },
+  shuffle: (): MusicQueueItemType[] => {
+    for (let i = queue.length - 1; i > 0; i--) {
+      const rand = Math.floor(Math.random() * (i + 1));
+      const oldElement = queue[i];
+      queue[i] = queue[rand];
+      queue[rand] = oldElement;
+    }
+    return queue;
+  }, 
   getIterator: () => {
     return queue.values();
   },
@@ -48,56 +57,3 @@ export const musicQueue = {
     return queue;
   },
 };
-
-// export const addSongRequest = (musicTitle: String, musicId: String): number => {
-//   return musicQueue.push({
-//     musicTitle: musicTitle,
-//     musicId: musicId,
-//   });
-// }
-
-// export const dequeue = (): {musicTitle: String, musicId: String} | undefined => {
-//   return musicQueue.shift();
-// }
-
-// export const swapQueuePositions = (positionA: number, positionB: number): boolean => {
-//   if (positionA <= 0 || positionB <= 0) {
-//     return false;
-//   } else if (musicQueue.at(positionA - 1) === undefined || musicQueue.at(positionB - 1) === undefined) {
-//     return false;
-//   }
-
-//   const tempMusicQueueValue = musicQueue[positionA - 1];
-//   musicQueue[positionA - 1] = musicQueue[positionB - 1];
-//   musicQueue[positionB - 1] = tempMusicQueueValue;
-//   return true;
-// }
-
-// export const removeQueueItem = (queuePos: number): MusicQueueItemType | undefined => {
-//   const deletedItemsArr = musicQueue.splice(queuePos - 1, 1);
-//   return deletedItemsArr.length === 0 ? undefined : deletedItemsArr[0];
-// };
-
-// export const removeQueueItemsRange = (queuePosA: number, queuePosB: number): MusicQueueItemType[] | undefined => {
-//   return queuePosA > queuePosB || queuePosA <= 0 || queuePosB <= 0 
-//     ? undefined
-//     : musicQueue.splice(queuePosA - 1, queuePosB - 1 - queuePosA + 2);
-// }
-
-// export const getMusicQueueIterator = () => {
-//   return musicQueue.values();
-// }
-
-// export const getMusicQueueItem = (index: number) => {
-//   if (index <= 0) return undefined;
-//   return musicQueue.at(index - 1);
-// }
-
-// export const getMusicQueueLength = (): number => {
-//   return musicQueue.length;
-// }
-
-// export const clearQueue = (): MusicQueueItemType[] => {
-//   musicQueue.length = 0;
-//   return musicQueue;
-// }
