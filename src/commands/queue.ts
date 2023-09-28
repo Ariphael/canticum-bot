432a15b5e78eba0013287689caf9b9b5d0df4ab5
Richard Tochtermann <richard.tochtermann@outlook.com>
2023-09-28 13:49:29 +0200

    FIX: changed the expression on line 31 so that pageOption is null if the user did not specify a specific page number

--- src/commands/queue.ts
+++ src/commands/queue.ts
@@ -28,7 +28,7 @@ export const queue: Command = {
 const executeQueue = async (_client: Client, interaction: ChatInputCommandInteraction<CacheType>) => {
   const queueMaxPage = Math.ceil(musicQueue.getLength() / resultsPerPage);
   const musicQueueLength = musicQueue.getLength();
-  const pageOption = interaction.options.get('page').value as number;
+  const pageOption = interaction.options.get('page') ? interaction.options.get('page').value as number : null;
   const pageNum = pageOption === null
     ? 0
     : Math.min(pageOption - 1, queueMaxPage);
