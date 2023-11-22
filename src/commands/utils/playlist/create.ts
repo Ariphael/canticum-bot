5ffcdec607459eb4cc5ca70f5f406e50c27bcb8b
Richard Tochtermann <richard.tochtermann@outlook.com>
2023-11-22 17:20:03 +0100

    NEW: created command 'playlist create' which constructs a new empty personal playlist for a user. Inserts into the playlist table of DB if given new personal playlist name is unique otherwise sets an appropriate embed message

---
+++ src/commands/utils/playlist/create.ts
@@ -0,0 +1,29 @@
+import { ChatInputCommandInteraction, CacheType, EmbedBuilder, userMention } from "discord.js";
+import * as db from '../../../utils/database';
+
+export const executePlaylistCreate = async (interaction: ChatInputCommandInteraction<CacheType>, embed: EmbedBuilder) => {
+  const memberId = interaction.member.user.id;
+  const playlistName = interaction.options.get('name').value as string;
+
+  embed.setTitle('Playlist');
+  
+  await db.query('SELECT * FROM playlist WHERE playlistName = ? AND userId = ?', [playlistName, memberId])
+    .then((result) => {
+      if (result.length === 0) {
+        db.query(
+          'INSERT INTO playlist (playlistName, userId) VALUES (?, ?)',
+          [playlistName, memberId]
+        ).then(_ => {
+          embed.setDescription(`Successfully created playlist ${playlistName}`)
+            .setTimestamp();
+          interaction.reply({ content: '', components: [], embeds: [embed] });
+        });
+      } else {
+        embed.setDescription(
+          `Playlist with name "${playlistName}" belonging to ${userMention(memberId)} already exists`
+        )
+          .setTimestamp();
+        interaction.reply({ content: '', components: [], embeds: [embed] });
+      }
+    });
+}
