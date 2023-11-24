925b54eb95347561f51b4953d8f35540f31a6945
Richard Tochtermann <richard.tochtermann@outlook.com>
2023-11-24 16:15:06 +0100

    NEW: added playlist remove item command which removes an entry in the playlist_content_map table and updates the relevant playlist positions to reflect the change if the given playlistName matches a playlist owned by the user. Sets an appropriate embed message no such playlist exists or item was successfully removed.

---
+++ src/commands/utils/playlist/removeitem.ts
@@ -0,0 +1,57 @@
+import { CacheType, ChatInputCommandInteraction, EmbedBuilder, userMention } from 'discord.js';
+import * as db from '../../../utils/database';
+
+export const executePlaylistRemoveItem = async (interaction: ChatInputCommandInteraction<CacheType>, embed: EmbedBuilder) => {
+  const memberId = interaction.member.user.id;
+  const playlistName = interaction.options.get('name').value as string;
+  const positionInPlaylist = interaction.options.get('position').value as number;
+
+  const numberOfPlaylistItemsQueryResult = await db.query(
+    `SELECT COUNT(*) AS itemCount FROM playlist_content_map WHERE userId = ? AND playlistName = ?`,
+    [memberId, playlistName]);
+  const numberOfPlaylistItems = numberOfPlaylistItemsQueryResult[0].itemCount;
+
+  if (numberOfPlaylistItems < positionInPlaylist || positionInPlaylist < 1) {
+    embed.setTitle('Error')
+      .setDescription('Invalid position value')
+      .setTimestamp();
+    return interaction.reply({ content: '', components: [], embeds: [embed], ephemeral: true });
+  }
+
+  db.query(
+    `SELECT * FROM playlist WHERE playlistName = ? AND userId = ?`,
+    [playlistName, memberId]
+  ).then(async (result) => {
+    if (result.length === 0) {
+      embed.setTitle('Error')
+        .setDescription(`No such playlist with name "${playlistName}" belonging to ${userMention(memberId)} was found.`)
+        .setTimestamp();
+      return interaction.reply({ content: '', components: [], embeds: [embed], ephemeral: true }); 
+    }  
+    const playlistItemTitle = await removeItemAndUpdatePositionsInDB(memberId, playlistName, positionInPlaylist);
+    embed.setTitle('Playlist')
+      .setDescription(`Removed ${playlistItemTitle} from position ${positionInPlaylist} of playlist ${playlistName}`)
+      .setTimestamp();
+    return interaction.reply({ content: '', components: [], embeds: [embed] });
+  })
+}
+
+const removeItemAndUpdatePositionsInDB = async (memberId: string, playlistName: string, positionInPlaylist: number) => {
+  return db.query(
+    `SELECT title FROM playlist_items INNER JOIN playlist_content_map USING (playlistItemId) WHERE userId = ? AND playlistName = ? AND playlistPosition = ?`,
+    [memberId, playlistName, positionInPlaylist])
+    .then(result => {
+      const itemTitle = result[0].title as string;
+      db.query(
+        `DELETE FROM playlist_content_map WHERE userId = ? AND playlistName = ? AND playlistPosition = ?`,
+        [memberId, playlistName, positionInPlaylist]
+      );
+    
+      db.query(
+        `UPDATE playlist_content_map SET playlistPosition = playlistPosition - 1 WHERE userId = ? AND playlistName = ? AND playlistPosition > ?`,
+        [memberId, playlistName, positionInPlaylist]
+      );
+
+      return itemTitle;
+    })
+}
