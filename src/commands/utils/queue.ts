96bba6f3b1c4b1c71e8743da4077b4e22f2b0827
Richard Tochtermann <richard.tochtermann@outlook.com>
2023-11-27 11:49:53 +0100

    FIX: refactored auxiliary functions in commands/utils/queue.ts to modify an embed passed in as a parameter instead of creating and building a new embed

--- src/commands/utils/queue.ts
+++ src/commands/utils/queue.ts
@@ -10,38 +10,53 @@ import {
 import { enqueueYouTubePlaylistRequest, enqueueYouTubeSongRequest } from './play/youtube';
 
 /*
- * Contains auxiliary functions involved with fetching and enqueuing music from YouTube/Spotify
+ * Contains auxiliary function responsible for fetching and enqueuing music from YouTube/Spotify
  */
+
+const youtubeURLRegExp = 
+  /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/;
+const spotifyURLRegExp = 
+  /^(?:spotify:|(?:https?:\/\/(?:open|play)\.spotify\.com\/))(?:embed)?\/?(playlist|track)(?::|\/)((?:[0-9a-zA-Z]){22})/;
 
 const failedYouTubeMusicQueryErrorStr = 'No song found associated with query';
 
-export const enqueueMusicYouTube = async (url: string): Promise<EmbedBuilder> => {
+export const enqueueMusicAndBuildEmbed = async (query: string, embed: EmbedBuilder) => {
+  const isQueryYouTubeURL = youtubeURLRegExp.test(query);
+  const isQuerySpotifyURL = spotifyURLRegExp.test(query);
+
+  return isQueryYouTubeURL
+    ? await enqueueMusicYouTube(query, embed)
+    : (isQuerySpotifyURL
+      ? await enqueueMusicSpotify(query, embed)
+      : await enqueueMusicYouTubeNonURLQuery(query, embed));
+}
+
+const enqueueMusicYouTube = async (url: string, embed: EmbedBuilder): Promise<EmbedBuilder> => {
   const includesPlaylistId = url.includes('list=');
 
   if (includesPlaylistId) {
     // do this for enqueueyoutubesongrequest
-    return await enqueueYouTubePlaylistRequest(url);
+    return await enqueueYouTubePlaylistRequest(url, embed);
   } else {
-    return await enqueueYouTubeSongRequest(url);
+    return await enqueueYouTubeSongRequest(url, embed);
   }
 }
 
-export const enqueueMusicSpotify = async (url: string): Promise<EmbedBuilder> => {
+const enqueueMusicSpotify = async (url: string, embed: EmbedBuilder): Promise<EmbedBuilder> => {
   const includesPlaylistId = url.includes('playlist/');
   const includesTrackId = url.includes('track/');
   const includesAlbumId = url.includes('album/');
 
   if (includesPlaylistId) {
-    return await enqueueSpotifyPlaylistRequest(url);
+    return await enqueueSpotifyPlaylistRequest(url, embed);
   } else if (includesTrackId) {
-    return await enqueueSpotifyTrackRequest(url);
+    return await enqueueSpotifyTrackRequest(url, embed);
   } else if (includesAlbumId) {
-    return await enqueueSpotifyAlbumRequest(url);
+    return await enqueueSpotifyAlbumRequest(url, embed);
   }
 }
 
-export const enqueueMusicYouTubeNonURLQuery = async (query: string): Promise<EmbedBuilder> => {
-  const embed = new EmbedBuilder();
+const enqueueMusicYouTubeNonURLQuery = async (query: string, embed: EmbedBuilder): Promise<EmbedBuilder> => {
   try {
     const videoInfo = await axios
       .get(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${query}&key=${process.env.YOUTUBE_API_KEY}`)
