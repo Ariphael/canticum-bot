c9d6c0db28b84f3527b7865589ba96d277ffbaf8
Richard Tochtermann <richard.tochtermann@outlook.com>
2023-12-13 17:55:15 +0100

    NEW: Modified playlist remove item subcommand by swapping error checking logic so an erroneous playlist name parameter is checked first, expanding position value error checking logic so error embed messages are more informative and altering removeItemAndUpdatePositionsInDB to return the title of the removed entry and inserting aliases into sql query (line 47) to improve clarity of query

--- src/commands/playlist/removeitem.ts
+++ src/commands/playlist/removeitem.ts
@@ -6,18 +6,6 @@ export const executePlaylistRemoveItem = async (interaction: ChatInputCommandInt
   const playlistName = interaction.options.get('name').value as string;
   const positionInPlaylist = interaction.options.get('position').value as number;
 
-  const numberOfPlaylistItemsQueryResult = await db.query(
-    `SELECT COUNT(*) AS itemCount FROM playlist_content_map WHERE userId = ? AND playlistName = ?`,
-    [memberId, playlistName]);
-  const numberOfPlaylistItems = numberOfPlaylistItemsQueryResult[0].itemCount;
-
-  if (numberOfPlaylistItems < positionInPlaylist || positionInPlaylist < 1) {
-    embed.setTitle('Error')
-      .setDescription('Invalid position value')
-      .setTimestamp();
-    return await interaction.reply({ content: '', components: [], embeds: [embed], ephemeral: true });
-  }
-
   const result = await db.query(
     `SELECT * FROM playlist WHERE playlistName = ? AND userId = ?`,
     [playlistName, memberId]
@@ -30,6 +18,23 @@ export const executePlaylistRemoveItem = async (interaction: ChatInputCommandInt
     return interaction.reply({ content: '', components: [], embeds: [embed], ephemeral: true });     
   }
 
+  const numberOfPlaylistItemsQueryResult = await db.query(
+    `SELECT COUNT(*) AS itemCount FROM playlist_content_map WHERE userId = ? AND playlistName = ?`,
+    [memberId, playlistName]);
+  const numberOfPlaylistItems = numberOfPlaylistItemsQueryResult[0].itemCount as number;
+
+  if (numberOfPlaylistItems < positionInPlaylist) {
+    embed.setTitle('Error')
+      .setDescription(`Position value too large`)
+      .setTimestamp();
+    return await interaction.reply({ content: '', components: [], embeds: [embed], ephemeral: true });
+  } else if (numberOfPlaylistItems < positionInPlaylist || positionInPlaylist < 1) {
+    embed.setTitle('Error')
+      .setDescription('Invalid position value.')
+      .setTimestamp();
+    return await interaction.reply({ content: '', components: [], embeds: [embed], ephemeral: true });
+  }
+
   const playlistItemTitle = await removeItemAndUpdatePositionsInDB(memberId, playlistName, positionInPlaylist);
   embed.setTitle('Playlist')
     .setDescription(`Removed ${playlistItemTitle} from position ${positionInPlaylist} of playlist ${playlistName}`)
@@ -39,11 +44,10 @@ export const executePlaylistRemoveItem = async (interaction: ChatInputCommandInt
 
 const removeItemAndUpdatePositionsInDB = async (memberId: string, playlistName: string, positionInPlaylist: number) => {
   const result = await db.query(
-    `SELECT title FROM playlist_items INNER JOIN playlist_content_map USING (playlistItemId) WHERE userId = ? AND playlistName = ? AND playlistPosition = ?`,
+    `SELECT pi.title AS title FROM playlist_items AS pi INNER JOIN playlist_content_map AS pcm USING (playlistItemId) WHERE pcm.userId = ? AND pcm.playlistName = ? AND pcm.playlistPosition = ?`,
     [memberId, playlistName, positionInPlaylist]
   );
-  
-  const itemTitle = result[0].title as string;
+
   db.query(
     `DELETE FROM playlist_content_map WHERE userId = ? AND playlistName = ? AND playlistPosition = ?`,
     [memberId, playlistName, positionInPlaylist]
@@ -53,4 +57,6 @@ const removeItemAndUpdatePositionsInDB = async (memberId: string, playlistName: 
     `UPDATE playlist_content_map SET playlistPosition = playlistPosition - 1 WHERE userId = ? AND playlistName = ? AND playlistPosition > ?`,
     [memberId, playlistName, positionInPlaylist]
   );
+
+  return result[0].title as string;
 }
