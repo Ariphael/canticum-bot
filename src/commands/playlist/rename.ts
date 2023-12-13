2bb4c4197a795ea611fc7a8a1df959eea655f3ac
Richard Tochtermann <richard.tochtermann@outlook.com>
2023-12-13 14:52:48 +0100

    FIX: Modified executePlaylistRename to use 'async-await' instead of 'Promise.then' to improve readability by making code look more synchronous

--- src/commands/playlist/rename.ts
+++ src/commands/playlist/rename.ts
@@ -6,36 +6,35 @@ export const executePlaylistRename = async (interaction: ChatInputCommandInterac
   const playlistName = interaction.options.get('name').value as string;
   const newPlaylistName = interaction.options.get('newname').value as string;
 
-  await db.query(
+  const result = await db.query(
     `SELECT * FROM playlist WHERE playlistName = ? AND userId = ?`, 
     [playlistName, memberId]
-  ).then((result) => {
-    if (result.length === 0) {
-      embed.setTitle('Error')
-        .setDescription(`No such playlist with name "${playlistName}" belonging to ${userMention(memberId)} exists`);
-      return interaction.reply({ content: '', components: [], embeds: [embed] });
-    }
+  );
 
-    db.query(
-      'INSERT INTO playlist (playlistName, userId) VALUES (?, ?)', 
-      [newPlaylistName, memberId]
-    ).then(_ => {
-      db.query(
-        'UPDATE playlist_content_map SET playlistName = ? WHERE playlistName = ? AND userId = ?', 
-        [newPlaylistName, playlistName, memberId]
-      );
-
-      db.query(
-        `DELETE FROM playlist WHERE playlistName = ?`,
-        [playlistName]
-      );
-    });
-
-    embed.setTitle('Playlist')
-      .setDescription(
-        `Successfully renamed playlist "${playlistName}" belonging to ${userMention(memberId)} to "${newPlaylistName}"`
-      );
+  if (result.length === 0) {
+    embed.setTitle('Error')
+      .setDescription(`No such playlist with name "${playlistName}" belonging to ${userMention(memberId)} exists`);
     return interaction.reply({ content: '', components: [], embeds: [embed] });
-  });
+  }
 
+  db.query(
+    'INSERT INTO playlist (playlistName, userId) VALUES (?, ?)', 
+    [newPlaylistName, memberId]
+  )
+    
+  db.query(
+    'UPDATE playlist_content_map SET playlistName = ? WHERE playlistName = ? AND userId = ?', 
+    [newPlaylistName, playlistName, memberId]
+  );
+
+  db.query(
+    `DELETE FROM playlist WHERE playlistName = ?`,
+    [playlistName]
+  );
+
+  embed.setTitle('Playlist')
+    .setDescription(
+      `Successfully renamed playlist "${playlistName}" belonging to ${userMention(memberId)} to "${newPlaylistName}"`
+    );
+  return interaction.reply({ content: '', components: [], embeds: [embed] });
 }
