2c8c7f069ae34b673e8f3cf02e02da816e5a259c
Richard Tochtermann <richard.tochtermann@outlook.com>
2023-12-13 14:35:40 +0100

    FIX: Changed executePlaylistLoadToQueue to utilise async-await instead of Promise.then so the code looks more procedural and adheres to the style of the rest of the codebase. Changed the non-existent playlist error message to be more clear.

--- src/commands/playlist/load.ts
+++ src/commands/playlist/load.ts
@@ -8,54 +8,53 @@ export const executePlaylistLoadToQueue = async (interaction: ChatInputCommandIn
   const playlistName = interaction.options.get('name').value as string;
   const musicPlayerInstance = MusicPlayer.getMusicPlayerInstance();
 
-  await db.query(
-    'SELECT * FROM playlist WHERE userId = ? AND playlistName = ?', 
+  const queryResult = await db.query(
+    'SELECT * FROM playlist WHERE userId = ? AND playlistName = ?',
     [memberId, playlistName]
-  ).then((result) => {
-    if (result.length === 0) {
-      embed.setTitle('Error')
-        .setDescription(`Playlist with name "${playlistName}" belonging to ${userMention(memberId)} does not exist.`)
-        .setTimestamp();
-      return interaction.reply({ content: '', components: [], embeds: [embed] });
-    }
+  );
 
-    const musicQueueOldLength = musicQueue.getLength();
-    loadPlaylistToQueue(interaction, embed, memberId, playlistName).then(_ => {
-      if (musicQueueOldLength === 0 && !musicPlayerInstance.isPlayingAudio())
-        musicPlayerInstance.playAudio();
-    });
+  if (queryResult.length === 0) {
+    embed.setTitle('Error')
+      .setDescription(`No playlist with name "${playlistName}" belonging to ${userMention(memberId)} exists.`)
+      .setTimestamp();
+    return interaction.reply({ content: '', components: [], embeds: [embed] });
+  }
+
+  const musicQueueOldLength = musicQueue.getLength();
+  loadPlaylistToQueue(interaction, embed, memberId, playlistName).then(_ => {
+    if (musicQueueOldLength === 0 && !musicPlayerInstance.isPlayingAudio())
+      musicPlayerInstance.playAudio();
   });
 }
 
-const loadPlaylistToQueue = (
+const loadPlaylistToQueue = async (
   interaction: ChatInputCommandInteraction<CacheType>, 
   embed: EmbedBuilder, 
   memberId: string, 
   playlistName: string
 ) => {
-  return db.query(
+  const result = await db.query(
     'SELECT pi.title, pi.uploader, pi.musicId, pi.originalURL FROM playlist_items AS pi INNER JOIN playlist_content_map AS pcm ON (pi.playlistItemId = pcm.playlistItemId) INNER JOIN playlist AS p ON (pcm.playlistName = p.playlistName AND pcm.userId = p.userId) WHERE p.userId = ? AND p.playlistName = ? ORDER BY pcm.playlistPosition',
     [memberId, playlistName]
-  ).then((result) => {
-    if (result.length === 0) {
-      embed.setTitle('Error')
-        .setDescription(`Playlist "${playlistName}" has no content to append to the queue`)
-        .setTimestamp();
-      return interaction.reply({ content: '', components: [], embeds: [embed] });
-    }
-    result.forEach((playlistItem) => {
-      musicQueue.enqueue({
-        musicTitle: playlistItem.title,
-        musicId: playlistItem.musicId,
-        uploader: playlistItem.uploader,
-        originalURL: playlistItem.originalURL,
-      })
-    });
-    embed.setTitle('Playlist')
-      .setDescription(
-        `Content of playlist "${playlistName}" has been appended to the queue. Use /queue to see current state of queue.`
-      )
+  );
+  if (result.length === 0) {
+    embed.setTitle('Error')
+      .setDescription(`Playlist "${playlistName}" has no content to append to the queue`)
       .setTimestamp();
     return interaction.reply({ content: '', components: [], embeds: [embed] });
-  })
+  }
+  result.forEach((playlistItem) => {
+    musicQueue.enqueue({
+      musicTitle: playlistItem.title,
+      musicId: playlistItem.musicId,
+      uploader: playlistItem.uploader,
+      originalURL: playlistItem.originalURL,
+    });
+  });
+  embed.setTitle('Playlist')
+    .setDescription(
+      `Content of playlist "${playlistName}" has been appended to the queue. Use /queue to see current state of queue.`
+    )
+    .setTimestamp();
+  return await interaction.reply({ content: '', components: [], embeds: [embed] });
 }
