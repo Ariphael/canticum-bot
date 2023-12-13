cc97d5929e3e9e6001aafdd771ce1f2fe246fe04
Richard Tochtermann <richard.tochtermann@outlook.com>
2023-12-13 15:48:30 +0100

    FIX: stored mysql password as an environment variable and modified the connection config in utils/database.ts to use this env variable instead of directly passing the password in

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -4,7 +4,7 @@ import { RowDataPacket } from 'mysql2';
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'root',
+  password: process.env['MYSQL_PASSWORD'],
   database: 'canticum',
 });
 
