f1c70719efb50020c63551771083704e912c4ad4
Richard Tochtermann <richard.tochtermann@outlook.com>
2023-12-14 11:09:13 +0100

    FIX: Put executePlaylistView database query and embed setting logic in a try-catch block so logging can occur and the user can be informed if the view subcommand fails

--- src/commands/playlist/view.ts
+++ src/commands/playlist/view.ts
@@ -4,29 +4,37 @@ import * as db from '../../utils/database';
 export const executePlaylistView = async (interaction: ChatInputCommandInteraction<CacheType>, embed: EmbedBuilder) => {
   const memberId = interaction.member.user.id;
 
-  const result = await db.query(
-    'SELECT playlistName, COUNT(playlistItemId) AS \'count\' FROM playlist AS p LEFT JOIN playlist_content_map AS pcm USING (playlistName) WHERE p.userId = ? GROUP BY playlistName', 
-    [memberId],
-  );
-
-  if (result.length === 0) {
-    embed.setTitle('Playlist')
-      .setDescription('No items found')
+  try {
+    const result = await db.query(
+      'SELECT playlistName, COUNT(playlistItemId) AS \'count\' FROM playlist AS p LEFT JOIN playlist_content_map AS pcm USING (playlistName) WHERE p.userId = ? GROUP BY playlistName', 
+      [memberId],
+    );
+  
+    if (result.length === 0) {
+      embed.setTitle('Playlist')
+        .setDescription('No items found')
+        .setTimestamp();
+    } else {
+      embed.setTitle('Playlist')
+      .setDescription(
+        `${result.length} playlist${result.length > 1 ? 's' : ''} belonging to ${userMention(memberId)}:`
+      )
       .setTimestamp();
-    return interaction.reply({ content: '', components: [], embeds: [embed] });
+  
+      result.forEach((row, index) => {
+        embed.addFields({
+          name: `${index + 1}.`,
+          value: `${row.playlistName} (${row.count})`
+        });
+      });
+    }
+  } catch (error) {
+    console.error(`Playlist view subcommand failed: ${error}`);
+    embed.setTitle('Error')
+      .setDescription('An error occurred while executing playlist view subcommand')
+      .setTimestamp();
+      return interaction.reply({ content: '', components: [], embeds: [embed], ephemeral: true });
   }
-  embed.setTitle('Playlist')
-    .setDescription(
-      `${result.length} playlist${result.length > 1 ? 's' : ''} belonging to ${userMention(memberId)}:`
-    )
-    .setTimestamp();
-
-  result.forEach((row, index) => {
-    embed.addFields({
-      name: `${index + 1}.`,
-      value: `${row.playlistName} (${row.count})`
-    });
-  });
 
   return interaction.reply({ content: '', components: [], embeds: [embed] });
 }
