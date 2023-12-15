fb27749f78167d46208218949d7dfa39e8877b91
Richard Tochtermann <richard.tochtermann@outlook.com>
2023-12-15 18:40:01 +0100

    FIX: used mysql traction-commit-rollback to prevent inconsistent state in playlist import subcommand, wrapped main playlist import subcommand logic in try-catch so failed queries can be logged and an error message can be sent to the user and added a condition checking that the user has not inputted a url to an  empty yt playlist

--- src/commands/playlist/import.ts
+++ src/commands/playlist/import.ts
@@ -11,43 +11,61 @@ export const executePlaylistImport = async (interaction: ChatInputCommandInterac
 
   var initialSizeOfPlaylist = await getNumberOfPlaylistItems(memberId, targetPlaylistName);
 
-  const result = await db.query(
-    `SELECT * FROM playlist WHERE playlistName = ? AND userId = ?`,
-    [targetPlaylistName, memberId]
-  );
+  await db.query('START TRANSACTION');
 
-  if (result.length === 0) {
-    await db.query(
-      'INSERT INTO playlist (playlistName, userId) VALUES (?, ?)',
+  try {
+    const result = await db.query(
+      `SELECT * FROM playlist WHERE playlistName = ? AND userId = ?`,
       [targetPlaylistName, memberId]
     );
+
+    if (playlistItems.length === 0) {
+      embed.setTitle('Playlist')
+        .setDescription('External playlist specified is empty.')
+        .setTimestamp();
+      return await interaction.reply({ content: '', components: [], embeds: [embed] });
+    }
+
+    if (result.length === 0) {
+      await db.query(
+        'INSERT INTO playlist (playlistName, userId) VALUES (?, ?)',
+        [targetPlaylistName, memberId]
+      );
+    }
+
+    for (const playlistItem of playlistItems) {
+      await db.query(
+        `INSERT INTO playlist_items (title, uploader, musicId, originalURL, dateCreated) VALUES (?, ?, ?, ? ,?)`,
+        [playlistItem.musicTitle, playlistItem.uploader, playlistItem.musicId, 
+          playlistItem.originalURL, playlistItem.dateCreated]
+      );
+
+      const result = await db.query(
+        'SELECT playlistItemId FROM playlist_items WHERE title = ? AND uploader = ? AND musicId = ? AND originalURL = ?',
+        [playlistItem.musicTitle, playlistItem.uploader, playlistItem.musicId, 
+          playlistItem.originalURL]
+      );
+
+      await db.query(
+        'INSERT INTO playlist_content_map (playlistItemId, userId, playlistName, playlistPosition) VALUES (?, ?, ?, ?)',
+        [result[0].playlistItemId, memberId, targetPlaylistName, initialSizeOfPlaylist + playlistItems.indexOf(playlistItem) + 1]
+      );
+    };
+
+    embed.setTitle('Import')
+      .setDescription(
+        `${playlistInfo.itemCount} items from YouTube playlist "${playlistInfo.playlistName}" appended to playlist "${targetPlaylistName}" belonging to ${userMention(memberId)}`
+      )
+      .setTimestamp();
+    await db.query('COMMIT');
+    return await interaction.reply({ content: '', components: [], embeds: [embed] });
+  } catch (error) {
+    console.error(`Transaction error: ${error}`);
+    embed.setTitle('Error')
+      .setDescription('An error occurred while importing a playlist.')
+      .setTimestamp();
+    return await interaction.reply({ content: '', components: [], embeds: [embed], ephemeral: true });
   }
-
-  playlistItems.forEach(async (playlistItem, playlistItemIndex) => {
-    await db.query(
-      `INSERT INTO playlist_items (title, uploader, musicId, originalURL, dateCreated) VALUES (?, ?, ?, ? ,?)`,
-      [playlistItem.musicTitle, playlistItem.uploader, playlistItem.musicId, 
-        playlistItem.originalURL, playlistItem.dateCreated]
-    );
-
-    const result = await db.query(
-      'SELECT playlistItemId FROM playlist_items WHERE title = ? AND uploader = ? AND musicId = ? AND originalURL = ?',
-      [playlistItem.musicTitle, playlistItem.uploader, playlistItem.musicId, 
-        playlistItem.originalURL]
-    );
-
-    await db.query(
-      'INSERT INTO playlist_content_map (playlistItemId, userId, playlistName, playlistPosition) VALUES (?, ?, ?, ?)',
-      [result[0].playlistItemId, memberId, targetPlaylistName, initialSizeOfPlaylist + playlistItemIndex + 1]
-    );
-  });
-
-  embed.setTitle('Import')
-  .setDescription(
-    `${playlistInfo.itemCount} items from YouTube playlist "${playlistInfo.playlistName}" appended to playlist "${targetPlaylistName}" belonging to ${userMention(memberId)}`
-  )
-  .setTimestamp();
-  return await interaction.reply({ content: '', components: [], embeds: [embed] });
 }
 
 const getNumberOfPlaylistItems = async (memberId: string, playlistName: string): Promise<number> => {
