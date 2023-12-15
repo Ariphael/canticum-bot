990cf8d7612fdcb7e3a6cf552724c6b4dde26afe
Richard Tochtermann <richard.tochtermann@outlook.com>
2023-12-15 19:04:25 +0100

    unimportant outstanding minor refactoring changes in playlist view subcommand method executePlaylistView

--- src/commands/playlist/view.ts
+++ src/commands/playlist/view.ts
@@ -9,18 +9,18 @@ export const executePlaylistView = async (interaction: ChatInputCommandInteracti
       'SELECT playlistName, COUNT(playlistItemId) AS \'count\' FROM playlist AS p LEFT JOIN playlist_content_map AS pcm USING (playlistName) WHERE p.userId = ? GROUP BY playlistName', 
       [memberId],
     );
-  
+
     if (result.length === 0) {
       embed.setTitle('Playlist')
         .setDescription('No items found')
         .setTimestamp();
     } else {
       embed.setTitle('Playlist')
-      .setDescription(
-        `${result.length} playlist${result.length > 1 ? 's' : ''} belonging to ${userMention(memberId)}:`
-      )
-      .setTimestamp();
-  
+        .setDescription(
+          `${result.length} playlist${result.length > 1 ? 's' : ''} belonging to ${userMention(memberId)}:`
+        )
+        .setTimestamp();
+
       result.forEach((row, index) => {
         embed.addFields({
           name: `${index + 1}.`,
@@ -33,7 +33,7 @@ export const executePlaylistView = async (interaction: ChatInputCommandInteracti
     embed.setTitle('Error')
       .setDescription('An error occurred while executing playlist view subcommand')
       .setTimestamp();
-      return interaction.reply({ content: '', components: [], embeds: [embed], ephemeral: true });
+    return interaction.reply({ content: '', components: [], embeds: [embed], ephemeral: true });
   }
 
   return interaction.reply({ content: '', components: [], embeds: [embed] });
