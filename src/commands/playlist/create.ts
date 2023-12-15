c0c75a3cca27e818808873f8781e460f4be201ec
Richard Tochtermann <richard.tochtermann@outlook.com>
2023-12-15 18:19:11 +0100

    FIX: wrapped playlist create subcommand logic in try-catch so failed database queries can be logged and an error message can be sent to user

--- src/commands/playlist/create.ts
+++ src/commands/playlist/create.ts
@@ -5,24 +5,30 @@ export const executePlaylistCreate = async (interaction: ChatInputCommandInterac
   const memberId = interaction.member.user.id;
   const playlistName = interaction.options.get('name').value as string;
 
-  embed.setTitle('Playlist');
+  try {
+    const result = await db.query('SELECT * FROM playlist WHERE playlistName = ? AND userId = ?', [playlistName, memberId]);
 
-  const result = await db.query('SELECT * FROM playlist WHERE playlistName = ? AND userId = ?', [playlistName, memberId]);
-
-  if (result.length === 0) {
-    db.query(
-      'INSERT INTO playlist (playlistName, userId) VALUES (?, ?)',
-      [playlistName, memberId]
-    ).then(_ => {
-      embed.setDescription(`Successfully created playlist ${playlistName}`)
+    if (result.length === 0) {
+      await db.query(
+        'INSERT INTO playlist (playlistName, userId) VALUES (?, ?)',
+        [playlistName, memberId]
+      )
+      embed.setTitle('Playlist')
+        .setDescription(`Successfully created playlist ${playlistName}`)
         .setTimestamp();
-      interaction.reply({ content: '', components: [], embeds: [embed] });
-    });
-  } else {
-    embed.setDescription(
-      `Playlist with name "${playlistName}" belonging to ${userMention(memberId)} already exists`
-    )
+      await interaction.reply({ content: '', components: [], embeds: [embed] });
+    } else {
+      embed.setDescription(
+        `Personal playlist with name "${playlistName}" already exists`
+      )
+        .setTimestamp();
+      await interaction.reply({ content: '', components: [], embeds: [embed] });
+    }
+  } catch (error) {
+    embed.setTitle('Error')
+      .setDescription('An error occurred while creating a playlist')
       .setTimestamp();
-    interaction.reply({ content: '', components: [], embeds: [embed] });
+    console.error(`Playlist create subcommand error: ${error}`);
+    await interaction.reply({ content: '', components: [], embeds: [embed], ephemeral: true });
   }
 }
