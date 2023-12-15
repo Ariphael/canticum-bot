b2c2dc9d9a96085cf0950e0ee770d825a2ba2ce0
Richard Tochtermann <richard.tochtermann@outlook.com>
2023-12-15 18:22:54 +0100

    FIX: wrapped playlist delete subcommand logic in try-catch so failed database query events can be handled

--- src/commands/playlist/delete.ts
+++ src/commands/playlist/delete.ts
@@ -5,21 +5,29 @@ export const executePlaylistDelete = async (interaction: ChatInputCommandInterac
   const memberId = interaction.member.user.id;
   const playlistName = interaction.options.get('name').value as string;
 
-  const result = await db.query(
-    'SELECT * FROM playlist WHERE playlistName = ? AND userId = ?', 
-    [playlistName, memberId]
-  );
+  try {
+    const result = await db.query(
+      'SELECT * FROM playlist WHERE playlistName = ? AND userId = ?', 
+      [playlistName, memberId]
+    );
 
-  if (result.length === 0) {
-    embed.setDescription(`No playlist with name ${playlistName} belonging to ${userMention(memberId)} was found`)
+    if (result.length === 0) {
+      embed.setTitle('Playlist')
+        .setDescription(`No playlist with name ${playlistName} belonging to ${userMention(memberId)} exists`)
+        .setTimestamp();
+      interaction.reply({ content: '', components: [], embeds: [embed] })
+    } else {
+      await db.query('DELETE FROM playlist WHERE playlistName = ? AND userId = ?', [playlistName, memberId])
+      embed.setTitle('Playlist')
+        .setDescription(`Successfully deleted playlist "${playlistName}" belonging to ${userMention(memberId)}`)
+        .setTimestamp();
+      await interaction.reply({ content: '', components: [], embeds: [embed] });
+    }
+  } catch (error) {
+    embed.setTitle('Error')
+      .setDescription('An error occurred while deleting a playlist.')
       .setTimestamp();
-    interaction.reply({ content: '', components: [], embeds: [embed] })
-  } else {
-    db.query('DELETE FROM playlist WHERE playlistName = ? AND userId = ?', [playlistName, memberId])
-      .then(_ => {
-        embed.setDescription(`Successfully deleted playlist "${playlistName}" belonging to ${userMention(memberId)}`)
-          .setTimestamp();
-        interaction.reply({ content: '', components: [], embeds: [embed] });
-      });
+    console.error(`Playlist delete subcommand error: ${error}`);
+    await interaction.reply({ content: '', components: [], embeds: [embed], ephemeral: true });
   }
 }
