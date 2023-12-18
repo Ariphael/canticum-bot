5dd9d0cefc4e8577db27338a148381e8830909bc
Richard Tochtermann <richard.tochtermann@outlook.com>
2023-12-18 18:47:39 +0100

    NEW: updated help embeds by removing pages for the embed utilised when the help command is used without the command option and updating names and descriptions of commands

--- src/embeds/helpEmbeds.ts
+++ src/embeds/helpEmbeds.ts
@@ -1,38 +1,37 @@
 import { commandData } from './helpCommandsInfo';
 import { EmbedBuilder } from 'discord.js';
 
-export const numPages = 2;
-export const embeds: EmbedBuilder[] = [
-  new EmbedBuilder() 
-    .setColor(0x0099FF)
-    .setTitle('Help')
-    .setAuthor({ name: 'Canticum' })
-    .setDescription('List of essential commands. Usage: /help [command]')
-    .addFields(
-      { name: 'help (command)', value: 'Displays this page/information about specific command', inline: true },
-      { name: 'ping', value: 'Displays bot and API latency in ms', inline: true} ,
-      { name: 'connect [target]', value: 'Connects to target voice channel', inline: true },
-      { name: 'disconnect', value: 'Disconnects from voice channel', inline: true}, 
-      { name: 'queue', value: 'Displays the queue', inline: true }, 
-      { name: 'play [link/query]', value: 'Plays/adds music to queue. Searches on youtube.', inline: true },
-      { name: 'clear', value: 'clears queue', inline: true },
-    )
-    .setTimestamp()
-    .setFooter({ text: `page 1/${numPages}`}), 
-  new EmbedBuilder() 
-    .setColor(0x0099FF)
-    .setTitle('Help')
-    .setAuthor({ name: 'Canticum' })
-    .setDescription('Test page. Usage: /help [command]')
-    .setTimestamp()
-    .setFooter({ text: `page 2/${numPages}`})    
-];
+
+export const mainHelpEmbed = new EmbedBuilder()
+  .setTitle('Help')
+  .setAuthor({ name: 'Canticum' })
+  .setDescription('List of commands. Usage: /help [command]')
+  .addFields(
+    { name: 'help (command)', value: 'Displays this page/information about specific command', inline: true },
+    { name: 'ping', value: 'Displays bot and API latency in ms', inline: true} ,
+    { name: 'connect [target]', value: 'Connects to target voice channel', inline: true },
+    { name: 'disconnect', value: 'Disconnects from voice channel', inline: true}, 
+    { name: 'queue', value: 'Displays the queue', inline: true }, 
+    { name: 'play (link/query)', value: 'Commences playback or appends music to queue', inline: true },
+    { name: 'clear', value: 'Clears queue', inline: true },
+    { name: 'loop', value: 'Toggles looping for currently playing song/queue', inline: true },
+    { name: 'nowplaying', value: 'Display song currently playing', inline: true },
+    { name: 'pause', value: 'Pause audio', inline: true },
+    { name: 'unpause', value: 'Unpause audio', inline: true },
+    { name: 'playlist', value: 'Manage/enqueue personal playlists', inline: true },
+    { name: 'remove', value: 'Remove item(s) from queue', inline: true },
+    { name: 'shuffle', value: 'Shuffle items in queue', inline: true },
+    { name: 'skip', value: 'Skip currently playing song', inline: true },
+    { name: 'swap', value: 'Swap 2 items in queue', inline: true },
+    { name: 'volume', value: 'Adjust volume of player', inline: true },
+    { name: 'history', value: 'Displays history of songs played', inline: true },
+  )
+  .setTimestamp();
 
 export const getCommandEmbed = (command: String): EmbedBuilder => {
   const commandInfo = commandData.find((data) => data.commandName === command);
   if (commandInfo === undefined) {
     return new EmbedBuilder() 
-      .setColor(0x0099FF)
       .setTitle('Help')
       .setAuthor({ name: 'Canticum' })
       .setDescription('Unknown command. Please use /help to see list of valid commands.')
@@ -40,7 +39,6 @@ export const getCommandEmbed = (command: String): EmbedBuilder => {
       .setFooter({ text: `page 1/1`})  
   }
   return new EmbedBuilder() 
-    .setColor(0x0099FF)
     .setTitle(`${commandInfo.commandOptions}`)
     .setAuthor({ name: 'Canticum' })
     .setDescription(`${commandInfo.commandDescription}`)
