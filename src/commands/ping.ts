aca767972d75d789af06b6a078a18a3afbdaeab9
Richard Tochtermann <richard.tochtermann@outlook.com>
2023-12-18 18:51:46 +0100

    FIX: fixed indentation on line 19 commands/ping.ts

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -16,7 +16,7 @@ export const Ping: Command = {
 
 const executePing = async (client: Client, interaction: ChatInputCommandInteraction<CacheType>): Promise<void> => {
   const embed = new EmbedBuilder()
-  .setTitle('Pong!');
+    .setTitle('Pong!');
 
   await interaction.reply({ content: '', components: [], embeds: [embed] });
 };
