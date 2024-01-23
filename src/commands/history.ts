ac97d31da21c5deeba66c9ed68a5df0bd77ae097
Richard Tochtermann <richard.tochtermann@outlook.com>
2024-01-23 15:56:11 +0100

    NEW: implemented queue subcommand of history, modified embed description and field values to improve clarity of result and added page number option to queue subcommand

--- src/commands/history.ts
+++ src/commands/history.ts
@@ -2,7 +2,7 @@ import * as db from '../utils/database';
 import { Client, ChatInputCommandInteraction, CacheType, Application, ApplicationCommandOptionType, EmbedBuilder, userMention } from "discord.js";
 import { Command } from "../interfaces/command-interface";
 
-const resultsPerPage = 10;
+const resultsPerPage = 5;
 
 export const history: Command = {
   name: "history",
@@ -27,6 +27,11 @@ export const history: Command = {
     name: "queue",
     description: "history of songs enqueued",
     options: [{
+      type: ApplicationCommandOptionType.Integer,
+      name: "page",
+      description: "page number",
+      required: false,
+    }, {
       type: ApplicationCommandOptionType.Boolean,
       name: "personal",
       description: "only show songs issuer of command enqueued",
@@ -44,11 +49,11 @@ const executeHistory = async (client: Client<boolean>, interaction: ChatInputCom
 
   if (subcommand === 'play')
     await executeHistoryPlay(client, interaction, embed);
-  else if (subcommand === 'personal')
+  else if (subcommand === 'queue')
     await executeHistoryQueue(client, interaction, embed);
 }
 
-const executeHistoryPlay = async (client: Client<boolean>, interaction: ChatInputCommandInteraction<CacheType>, embed: EmbedBuilder) => {
+const executeHistoryPlay = async (_client: Client<boolean>, interaction: ChatInputCommandInteraction<CacheType>, embed: EmbedBuilder) => {
   const memberId = interaction.member.user.id;
   const personalOption = interaction.options.get('personal') 
     ? interaction.options.get('personal').value as boolean
@@ -79,13 +84,15 @@ const executeHistoryPlay = async (client: Client<boolean>, interaction: ChatInpu
   const startResultIndex = Math.min(historyMaxPage - 1, pageOption) * resultsPerPage;
 
   embed.setTitle('History')
-    .setDescription(`History of music played (${resultsPerPage} items per page)`);
+    .setDescription(
+      `History of music played ${personalOption ? `(Only songs enqueued by ${userMention(memberId)}) ` : ''}(${resultsPerPage} items per page)`
+    );
 
   Array.from(dequeueHistoryQueryResult.slice(startResultIndex, startResultIndex + resultsPerPage))
     .forEach((result, index) => {
       embed.addFields({
         name: `${index + startResultIndex + 1}.`,
-        value: `${result.title}\nEnqueued by ${userMention(result.memberId)}\nPlayed on ${result.timestamp}}`
+        value: `Title: ${result.title}\nEnqueued by: ${userMention(result.memberId)}\nPlayed on: ${result.timestamp}}`
       });
     });
   
@@ -97,6 +104,53 @@ const executeHistoryPlay = async (client: Client<boolean>, interaction: ChatInpu
   return interaction.reply({ content: '', components: [], embeds: [embed] });
 }
 
-const executeHistoryQueue = async (client: Client<boolean>, interaction: ChatInputCommandInteraction<CacheType>, embed: EmbedBuilder) => {
+const executeHistoryQueue = async (_client: Client<boolean>, interaction: ChatInputCommandInteraction<CacheType>, embed: EmbedBuilder) => {
+  const memberId = interaction.member.user.id;
+  const pageOption = interaction.options.get('page')
+    ? Math.max(0, (interaction.options.get('page').value as number) - 1)
+    : 0;
+  const personalOption = interaction.options.get('personal')
+    ? interaction.options.get('personal').value as boolean
+    : false;
 
+  const enqueueHistoryQueryResult = personalOption
+    ? await db.query(
+      'SELECT title, uploader, originalURL, userId, enqueueTimestamp FROM enqueue_history WHERE userId = ? ORDER BY enqueueTimestamp DESC',
+      [memberId]
+    )
+    : await db.query(
+      'SELECT title, uploader, originalURL, userId, enqueueTimestamp FROM enqueue_history ORDER BY enqueueTimestamp DESC'
+    );
+
+  if (enqueueHistoryQueryResult.length === 0) {
+    embed.setTitle('Error')
+      .setDescription(personalOption
+        ? 'You have no enqueued any song. Enqueue a song with /play'
+        : 'No queue history to show.');
+    return interaction.reply({ content: '', components: [], embeds: [embed] });
+  }
+
+  const historyMaxPage = Math.ceil(enqueueHistoryQueryResult.length / resultsPerPage);
+  const pageNumber = pageOption <= 0 ? 1 : Math.min(pageOption + 1, historyMaxPage);
+  const startResultIndex = Math.min(historyMaxPage - 1, pageOption) * resultsPerPage;
+
+  embed.setTitle('History')
+    .setDescription(
+      `History of songs enqueued ${personalOption ? `by ${userMention(memberId)} ` : ''}(${resultsPerPage} items per page)`
+    );
+
+  Array.from(enqueueHistoryQueryResult.slice(startResultIndex, startResultIndex + resultsPerPage))
+    .forEach((result, index) => {
+      embed.addFields({
+        name: `${index + startResultIndex + 1}.`,
+        value: `Title: ${result.title}\nEnqueued by: ${userMention(memberId)}\nEnqueue date: ${result.enqueueTimestamp}`
+      });
+    });
+
+  embed.setTimestamp()
+    .setFooter({
+      text: `Page ${pageNumber}/${historyMaxPage}`
+    });
+
+  return interaction.reply({ content: '', components: [], embeds: [embed] });
 }
