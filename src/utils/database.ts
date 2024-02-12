2f390e694314237fcb98dda702ac8a784eab9eac
Richard Tochtermann <richard.tochtermann@outlook.com>
2024-02-12 11:23:56 +1100

    FIX: refactored functions in src/utils/database.ts to utilise the mysql2/promise module instead and refactored query function to return a promise that resolves if the result of the query is a mysql.RowDataPacket[]. The latter was achieved by implementing a guard clause isRowDataPacket

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,14 +1,14 @@
-import * as mysql from 'mysql';
-import { RowDataPacket } from 'mysql2';
+import * as mysql from 'mysql2/promise';
+import { QueryCallbackResultBaseType } from '../types/QueryCallbackResultBaseType';
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: 'mysql',
   user: 'root',
   password: process.env['MYSQL_PASSWORD']!,
   database: 'canticum',
 });
 
-export const init = () => connection.connect((connectError) => {
+export const init = async () => (await connection).connect().catch((connectError) => {
   if (connectError) {
     console.error(
       `MySQL connection error. Please ensure you start the MySQL server and connection configuration parameters are correct.`
@@ -17,20 +17,22 @@ export const init = () => connection.connect((connectError) => {
   }
 });
 
-export const query = (sqlInput: string, args: Array<string | number> = []): Promise<RowDataPacket[]> => {
-  return new Promise<RowDataPacket[]>((success, reject) => {
-    connection.query(sqlInput, args, (queryError, result) => {
-      if (queryError) {
-        console.error(`MYSQL error: ${queryError.message}`);
-        reject(result);
-      }
-      else
-        success(result);
-    });
+export const query = async (sqlInput: string, args: Array<string | number> = []): Promise<mysql.RowDataPacket[]> => {
+  return new Promise(async (resolve, reject) => {
+    const queryResult = await (await connection).query(sqlInput, args);
+    if (isRowDataPacket(queryResult[0]))
+      resolve(queryResult[0] as mysql.RowDataPacket[]);
+    reject();
   });
 }
 
-export const exit = () => connection.end((endError) => {
+export const exit = async () => (await connection).end().catch((endError) => {
   if (endError)
     throw endError;
-})
+});
+
+const isRowDataPacket = (rows: QueryCallbackResultBaseType): rows is mysql.RowDataPacket[] => {
+  return Array.isArray(rows) 
+    && !rows.every(Array.isArray) 
+    && rows.every(row => rows.constructor.name !== 'ResultSetHeader');
+}
